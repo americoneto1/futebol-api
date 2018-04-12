c19eb29ff021b604f564db40b64e2fdc8115fb0c
Americo Neto <americoneto1@gmail.com>
2018-04-12 00:29:31 -0300

    fix(redis): flag enable

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 process.env.PORT = process.env.PORT || 9000;
 process.env.REDIS_ENABLE = process.env.REDIS_ENABLE || true;
 
-if (process.env.REDIS_ENABLE) {
+if (process.env.REDIS_ENABLE === true) {
     const cache = require('express-redis-cache')(require('./config/redis'));
     app.use(cache.route());
 }
